aa79371735f1e42a52fb1d604b9330ca4a6a79dc
Pedro Maia <pedroasmaia@outlook.com>
2023-02-01 13:53:43 -0300

    Correção os_type

--- ServicePlan.tf
+++ ServicePlan.tf
@@ -2,7 +2,7 @@ resource "azurerm_service_plan" "plan" {
   name = var.plan_name
   resource_group_name = var.rg_name
   location = var.location
-  os_type = "windows"
+  os_type = "Windows"
   sku_name = "Y1"
   tags = var.tags
 }
